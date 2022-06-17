c4d99f626c4bf8292c7ab73e1a583edcb4afadf8
Daniel_Garcia <dangarflo12@gmail.com>
2022-06-17 13:58:12 +0200

    1

---
+++ Ejercicio 6.cs
@@ -0,0 +1,31 @@
+Public class revista {
+
+public string Titulo_de_la_revista:;
+public string Tipo_de_revista: ;
+public string Autor: ;
+public string Edicion: ;
+{
+public int Reservar(fecha) {
+return fecha;
+}
+public int Entregar(fecha){
+return fecha;
+}
+}
+}
+
+
+Public class Lector {
+
+public string Nombre: ;
+public string Dirección: ;
+public int Teléfono: ;
+{
+public int Reservar(material){
+    return material 
+}
+public int Entregar(material){
+    return material
+}
+}
+}
